3f66116c507c6ce0d92b2404959e4661e9496b83
Chris Mears <chris.mears@gmail.com>
2018-01-19 11:22:18 -0800

    Add test lambda

---
+++ js/spam-filter.js
@@ -0,0 +1,10 @@
+exports.handler = function(event, context, callback) {
+  console.log(event);
+  callback(
+    null,
+    {
+      statusCode: 200,
+      body: JSON.stringify(event)
+    }
+  );
+}
